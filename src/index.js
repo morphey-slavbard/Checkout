3e4005429a83566b89ff6dd9f683dba8c54d987e
morphey-slavbard <81485841+morphey-slavbard@users.noreply.github.com>
2022-05-09 10:26:12 +0100

    Added Opti script

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import Checkout from "./Checkout";
 import "./styles.css";
 
+<script src="https://unpkg.com/@optimizely/optimizely-sdk/dist/optimizely.browser.umd.min.js"></script>
+
+
 function App() {
   return <Checkout />;
 }
