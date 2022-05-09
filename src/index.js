e8d7641cb01e30d0dff6e71b8606c7baba4b6f1d
morphey-slavbard <81485841+morphey-slavbard@users.noreply.github.com>
2022-05-09 10:27:06 +0100

    Removed opti Script

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import Checkout from "./Checkout";
 import "./styles.css";
 
-<script src="https://unpkg.com/@optimizely/optimizely-sdk/dist/optimizely.browser.umd.min.js"></script>
-
-
 function App() {
   return <Checkout />;
 }
